7095c7a479c8340415ba497541267f4b2a2f9154
Yogesh Vakhre <yogeshvakhre05@gmail.com>
2024-08-13 18:45:05 +0530

    added auth middelware

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,6 +11,16 @@ use DataTables;
 class PostController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
      * Display a listing of the resource.
      */
     public function index(Request $request)
